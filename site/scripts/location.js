1f07f379e2235a16b43a48d516fdf0272eabed9f
talrez <tal@way2cu.com>
2017-01-12 13:59:03 +0200

    Updated zoom param

--- site/scripts/location.js
+++ site/scripts/location.js
@@ -9,7 +9,7 @@ Site.Map = function() {
 
 	self._init = function() {
 		self.maps = new google.maps.Map(document.getElementById('map'),
-			{zoom: 10, mapTypeControl: false, draggable: false, scrollwheel: false});
+			{zoom: 18, mapTypeControl: false, draggable: false, scrollwheel: false});
 		self.geocoder = new google.maps.Geocoder();
 
 		var address = document.querySelector('input[type="hidden"]').value;
